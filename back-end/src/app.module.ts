fa48b5a9be61bf42c10b0e5bf719f3f4fa2134aa
Aleminkov <fukytor@gmail.com>
2025-03-22 19:53:44 +0300

    Пока добавил все нужные ресуры в бэк

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -6,9 +6,12 @@ import { ConfigModule } from '@nestjs/config'
 import { UserModule } from './user/user.module'
 import { ProductModule } from './product/product.module'
 import { PaginationModule } from './pagination/pagination.module';
+import { ReviewModule } from './review/review.module';
+import { OrderModule } from './order/order.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-	imports: [ConfigModule.forRoot(), AuthModule, UserModule, ProductModule, PaginationModule],
+	imports: [ConfigModule.forRoot(), AuthModule, UserModule, ProductModule, PaginationModule, ReviewModule, OrderModule, CategoryModule],
 	controllers: [AppController],
 	providers: [AppService]
 })
