664071fb10616e300a32110ebbb6df8bf1618701
Aleminkov <fukytor@gmail.com>
2025-03-22 10:43:26 +0300

    Взял слаг из интернета

--- back-end/src/product/product.service.ts
+++ back-end/src/product/product.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { PaginationService } from 'src/pagination/pagination.service';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+    constructor(private prisma:PrismaService, private pagination:PaginationService){}
+}
