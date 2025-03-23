d946f2bbfb9287e1ac3df8078a9e187f4c395d15
Aleminkov <fukytor@gmail.com>
2025-03-23 14:08:06 +0300

    Сделал категорию

--- back-end/src/category/category.controller.ts
+++ back-end/src/category/category.controller.ts
@@ -1,7 +1,58 @@
-import { Controller } from '@nestjs/common';
-import { CategoryService } from './category.service';
+import {
+	Body,
+	Controller,
+	Delete,
+	Get,
+	HttpCode,
+	Param,
+	Post,
+	Put,
+	UsePipes,
+	ValidationPipe
+} from '@nestjs/common'
+import { CategoryService } from './category.service'
+import { CategoryDto } from 'src/dto/category.dto'
+import { Auth } from 'src/decorators/auth.decorator'
 
 @Controller('category')
 export class CategoryController {
-  constructor(private readonly categoryService: CategoryService) {}
+	constructor(private readonly categoryService: CategoryService) {}
+
+	@Get()
+	async getAll() {
+		return this.categoryService.getAll()
+	}
+
+	@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+	@Put(':id')
+	@Auth()
+	async update(@Param('id') id: string, @Body() dto: CategoryDto) {
+		return this.categoryService.update(+id, dto)
+	}
+
+	@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+	@Auth()
+	@Post()
+	async create(@Body() dto: CategoryDto) {
+		return this.categoryService.create(dto)
+	}
+
+	@HttpCode(200)
+	@Delete(':id')
+	@Auth()
+	async delete(@Param('id') id: string) {
+		return this.categoryService.delete(+id)
+	}
+
+	@Get('by-slug/:slug')
+	async bySlug(slug: string) {
+		return this.categoryService.bySlug(slug)
+	}
+
+	@Get(':id')
+	async byId(id: string) {
+		return this.categoryService.byId(+id)
+	}
 }
--- back-end/src/category/category.service.ts
+++ back-end/src/category/category.service.ts
@@ -1,4 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common'
+import { CategoryDto } from 'src/dto/category.dto'
+import { PrismaService } from 'src/prisma.service'
+import { categoryReturnObject } from 'src/returnObject/return-category'
+import { generateSlug } from 'src/utils/slug-generate'
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+	constructor(private prisma: PrismaService) {}
+
+	async byId(id: number) {
+		const category = await this.prisma.category.findUnique({
+			where: { id },
+			select: categoryReturnObject
+		})
+		if (!category) throw new NotFoundException('Category not found')
+
+		return category
+	}
+
+	async bySlug(slug: string) {
+		const category = this.prisma.category.findUnique({
+			where: { slug },
+			select: categoryReturnObject
+		})
+		if (!category) throw new NotFoundException('Category not found!')
+
+		return category
+	}
+
+	async getAll() {
+		return await this.prisma.category.findMany({ select: categoryReturnObject })
+	}
+
+	async create(dto: CategoryDto) {
+		const newProduct = await this.prisma.category.create({
+			data: {
+				name: dto.name,
+				slug: generateSlug(dto.name)
+			}
+		})
+		return { message: 'Product created successful', newProduct }
+	}
+
+	async update(id: number, dto: CategoryDto) {
+		const product = await this.prisma.category.update({
+			where: { id },
+			data: { name: dto.name, slug: generateSlug(dto.name) }
+		})
+		return product
+	}
+	async delete(id: number) {
+		return await this.prisma.category.delete({ where: { id } })
+	}
+}
